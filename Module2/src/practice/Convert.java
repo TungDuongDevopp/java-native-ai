53dc2cd4739e10747500663f877aecba9481115a
TungDuongDevopp <xaydaa8@gmail.com>
2026-08-28 15:23:17 +0700

    Convert Decimal to Binary

---
+++ Module2/src/practice/Convert.java
@@ -0,0 +1,28 @@
+package practice;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Convert {
+
+    private final Deque<Integer> stack = new ArrayDeque<>();
+
+
+    public String convert(int num) {
+        StringBuilder sb = new StringBuilder();
+
+        while(num>0) {
+            stack.push(num%2);
+            num /=2;
+        }
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Convert convert = new Convert();
+        System.out.println(convert.convert(50));
+    }
+}
