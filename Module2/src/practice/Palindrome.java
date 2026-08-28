0dee834c3e4a363b9410f19f49ccd66d8216a900
TungDuongDevopp <xaydaa8@gmail.com>
2026-08-28 15:32:04 +0700

    Palindrome

---
+++ Module2/src/practice/Palindrome.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class Palindrome {
+
+    public boolean isPalindrome(String str) {
+    if(str.isBlank()) return false;
+    str = str.toLowerCase().trim();
+
+    int left = 0;
+    int right = str.length() - 1;
+    while(left < right) {
+        char leftChar = str.charAt(left);
+        char rightChar = str.charAt(right);
+        if(leftChar != rightChar) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "Able was I ere I saw Elbao";
+        Palindrome palindrome = new Palindrome();
+        System.out.println(palindrome.isPalindrome(str));
+    }
+}
