cfcb0ecb415ad310a22fe36812ffc11f8a46d4d9
TungDuongDevopp <xaydaa8@gmail.com>
2026-08-30 14:18:23 +0700

    ProductBuilder

---
+++ Module2/src/practice/ProductBuilder.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Product {
+    String name;
+    int price;
+}
+public class ProductBuilder {
+    public static void main(String[] args) {
+        // Yêu cầu: Tạo danh sách đối tượng sản phẩm từ mảng tên cho trước
+        String[] productNames = {"Áo thun", "Mũ lưỡi trai", "Quần Jean"};
+        List<Product> productList = new ArrayList<>();
+
+
+        for (String productName : productNames) {
+            Product tempProduct = new Product();
+            tempProduct.name = productName;
+            tempProduct.price = 100;
+
+            // Thêm sản phẩm vào danh sách
+            productList.add(tempProduct);
+        }
+
+        // In kết quả ra màn hình
+        for (Product p : productList) {
+            System.out.println("{ name: '" + p.name + "', price: " + p.price + " }");
+        }
+    }
+}
--- Module2/src/test/TriangleClassifierTest.java
+++ Module2/src/test/TriangleClassifierTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import test.model.TriangleClassifier;
