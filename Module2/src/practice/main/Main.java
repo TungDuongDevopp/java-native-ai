e563728c1c9307bb74938797abfcb7644ac5619f
TungDuongDevopp <xaydaa8@gmail.com>
2026-09-01 20:51:55 +0700

    MemoryPool

--- Module2/src/practice/main/Main.java
+++ Module2/src/practice/main/Main.java
@@ -1,32 +1,13 @@
 package practice.main;
-
-import practice.factory.CircleFactory;
-import practice.factory.RectangleFactory;
-import practice.factory.ShapeFactory;
-import practice.factory.SquareFactory;
-import practice.interfaces.IShape;
+import practice.model.Bullet;
+import practice.model.Gun;
 
 public class Main {
     public static void main(String[] args) {
-        ShapeFactory factory = new CircleFactory(5);
-        IShape circle = factory.createShape();
-        System.out.println("Name: "+ circle.getName());
-        circle.draw();
-        System.out.println("Chu vi hình tròn: " + circle.getPerimeter());
-        System.out.println("Diện tích hình tròn: " + circle.getArea());
-        System.out.println("=========================================");
-        factory = new RectangleFactory(5,4);
-        IShape rec = factory.createShape();
-        System.out.println("Name: "+ rec.getName());
-        rec.draw();
-        System.out.println("Chu vi hình chữ nhật: " + rec.getPerimeter());
-        System.out.println("Diện tích hình chữ nhat: " + rec.getArea());
-        System.out.println("=========================================");
-        factory = new SquareFactory(5);
-        IShape sq = factory.createShape();
-        System.out.println("Name: "+ sq.getName());
-        sq.draw();
-        System.out.println("Chu vi hình vuông: " + sq.getPerimeter());
-        System.out.println("Diện tích hình vuông: " + sq.getArea());
+        Gun gun = new Gun();
+        System.out.println("Start");
+        gun.fireInPool();
+        System.out.println("Game over");
+        System.out.println("Tocal bullet created: " + Bullet.count);
     }
 }
